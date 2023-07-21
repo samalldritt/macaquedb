91f93e0c4e2279a56f92b3f9c982b2e75e433f74
samalldritt <samalldritt73@gmail.com>
2023-07-21 13:06:58 -0400

    final packaged version

--- macaquedb/Database.py
+++ macaquedb/Database.py
@@ -1,13 +1,14 @@
-## This file will be for connecting to the database, building, pulling data, etc.
+# This file will be for connecting to the database, building, pulling data, etc.
 
 import os
 import sqlite3
 import re
 import uuid
-from Subject import Subject
-from Session import Session
-from Image import Image
-from utilities import *
+from .database.Subject import Subject
+from .database.Session import Session
+from .database.Image import Image
+from .database.utilities import *
+
 
 class Database:
 
@@ -18,55 +19,62 @@ class Database:
     def connect(self):
         self.conn = sqlite3.connect(self.db_path)
         self.cursor = self.conn.cursor()
-    
+
     '''
     Inserting data through a directory (populating subject tables, session tables, image tables, JSON tables)
     '''
+
     def input(self, data_dir):
         # Inputting data into the table into table
         self.insert_data(data_dir)
         return
-    
+
     '''
     Loop through all subjects in a 'site-' dir and populate or update into the database
     '''
+
     def insert_data(self, data_dir):
         site_name = os.path.basename(data_dir)
         for subject in os.listdir(data_dir):
             subject_dir = os.path.join(data_dir, subject)
-            if os.path.isdir(subject_dir) and subject.startswith("sub-"):  # Check if the item is a directory
+            # Check if the item is a directory
+            if os.path.isdir(subject_dir) and subject.startswith("sub-"):
                 subject_id = subject[len('sub-'):]
-                session_count = self.loop_sessions(subject_dir=subject_dir, site_name=site_name, subject_id=subject_id)
-                new_sub = Subject(subject_id=subject_id, 
-                                  site=site_name, 
+                session_count = self.loop_sessions(
+                    subject_dir=subject_dir, site_name=site_name, subject_id=subject_id)
+                new_sub = Subject(subject_id=subject_id,
+                                  site=site_name,
                                   sessions=session_count)
                 self.insert_subject(new_sub)
-    
+
     '''
     Loop over subjects in dir
     Returns session count for one subject
     '''
+
     def loop_sessions(self, subject_dir, site_name, subject_id):
         session_count = 0
         for session in os.listdir(subject_dir):
             session_path = os.path.join(subject_dir, session)
             if os.path.isdir(session_path) and session.startswith('ses-'):
                 unique_id = subject_id + '_' + session
-                ## Also loop all the images within this session
-                image_count = self.loop_images(session_dir=session_path, subject_id=subject_id, session_id=unique_id, site=site_name)  
+                # Also loop all the images within this session
+                image_count = self.loop_images(
+                    session_dir=session_path, subject_id=subject_id, session_id=unique_id, site=site_name)
                 new_session = Session(session_id=unique_id,
                                       subject_id=subject_id,
                                       site=site_name,
                                       image_count=image_count)
                 self.insert_session(new_session)
-                
+
                 session_count += 1
-        
+
         return session_count
-    
+
     '''
     Loop over NIFTI files in a directory and add them all to the database
     '''
+
     def loop_images(self, session_dir, subject_id, session_id, site):
         # Walk through every file in session (anat, dwi, fieldmap, func)
         image_count = 1
@@ -74,7 +82,7 @@ class Database:
             for file in files:
                 file_path = os.path.join(root, file)
                 abs_file_path = os.path.abspath(file)
-                ## Get file type and subtype
+                # Get file type and subtype
                 image_type = os.path.basename(root)
 
                 if image_type == "anat":
@@ -91,7 +99,7 @@ class Database:
                         image_subtype = "unknown"
                 if is_nifti_file(file_path):
                     new_image = Image(image_id=file,
-                                      session_id=session_id, 
+                                      session_id=session_id,
                                       subject_id=subject_id,
                                       image_type=os.path.basename(root),
                                       image_subtype=image_subtype,
@@ -100,37 +108,39 @@ class Database:
                                       site=site)
                     self.insert_image(new_image)
                     image_count += 1
-            
+
         return image_count
-    
+
     '''
     Function to add an image to the database
     '''
+
     def insert_image(self, image):
-        
-        ## Check if it is in there already
+
+        # Check if it is in there already
         query_check = "SELECT * FROM Image WHERE image_id = ?"
         self.cursor.execute(query_check, (image.image_id,))
         existing_image = self.cursor.fetchone()
-        
+
         attr_names = tuple(image.__dict__.keys())
-        
+
         if existing_image:
             print("Image already exists in the database:")
             print("Existing row:", existing_image)
-            
+
             new_row_values = tuple(getattr(image, attr) for attr in attr_names)
             print("New row:", new_row_values)
-            
+
             # Check if all attributes are the same
-            all_same = all(existing_image[i] == getattr(image, attr) for i, attr in enumerate(attr_names))
-            
+            all_same = all(existing_image[i] == getattr(
+                image, attr) for i, attr in enumerate(attr_names))
+
             if all_same:
                 print("Image is already in database with same attributes, skipping")
                 return
-            
+
             choice = input("Do you want to update the existing image? (y/n)")
-            
+
             if choice.lower() == "y":
                 query_update = f"UPDATE Image SET {', '.join(f'{name} = ?' for name in attr_names)} WHERE image_id = ?"
                 values = new_row_values + (image.image_id,)
@@ -145,36 +155,38 @@ class Database:
             self.cursor.execute(query_insert, values)
             self.conn.commit()
             print(f"Image {image.image_id} inserted successfully.")
-        
-    
+
     '''
     Adding a specific session to the database
     '''
+
     def insert_session(self, session):
-        
-        ## Check if it is in there already
+
+        # Check if it is in there already
         query_check = "SELECT * FROM Session WHERE session_id = ?"
         self.cursor.execute(query_check, (session.session_id,))
         existing_session = self.cursor.fetchone()
-        
+
         attr_names = tuple(session.__dict__.keys())
-        
+
         if existing_session:
             print("Session already exists in the database:")
             print("Existing row:", existing_session)
-            
-            new_row_values = tuple(getattr(session, attr) for attr in attr_names)
+
+            new_row_values = tuple(getattr(session, attr)
+                                   for attr in attr_names)
             print("New row:", new_row_values)
-            
+
             # Check if all attributes are the same
-            all_same = all(existing_session[i] == getattr(session, attr) for i, attr in enumerate(attr_names))
-            
+            all_same = all(existing_session[i] == getattr(
+                session, attr) for i, attr in enumerate(attr_names))
+
             if all_same:
                 print("Session has no columns to update, skipping")
                 return
-            
+
             choice = input("Do you want to update the existing session? (y/n)")
-            
+
             if choice.lower() == "y":
                 query_update = f"UPDATE Session SET {', '.join(f'{name} = ?' for name in attr_names)} WHERE session_id = ?"
                 values = new_row_values + (session.session_id,)
@@ -189,32 +201,36 @@ class Database:
             self.cursor.execute(query_insert, values)
             self.conn.commit()
             print(f"Session {session.session_id} inserted successfully.")
-        
+
     '''
     Takes in a subject class and inserts it into the database (checking whether it needs to be updated first)
     '''
+
     def insert_subject(self, subject):
         query_check = "SELECT * FROM Subject WHERE subject_id = ?"
         self.cursor.execute(query_check, (subject.subject_id,))
         existing_subject = self.cursor.fetchone()
-        
+
         attr_names = tuple(subject.__dict__.keys())
 
         if existing_subject:
             print("Subject already exists in the database:")
             print("Existing row:", existing_subject)
 
-            new_row_values = tuple(getattr(subject, attr) for attr in attr_names)
+            new_row_values = tuple(getattr(subject, attr)
+                                   for attr in attr_names)
             print("New row:", new_row_values)
-            
+
             # Check if all attributes are the same
-            all_same = all(existing_subject[i] == getattr(subject, attr) for i, attr in enumerate(attr_names))
-            
+            all_same = all(existing_subject[i] == getattr(
+                subject, attr) for i, attr in enumerate(attr_names))
+
             if all_same:
                 print("Subject has no columns to update, skipping")
                 return
-            
-            choice = input("Do you want to update the existing subject? (y/n): ")
+
+            choice = input(
+                "Do you want to update the existing subject? (y/n): ")
 
             if choice.lower() == 'y':
                 query_update = f"UPDATE Subject SET {', '.join(f'{name} = ?' for name in attr_names)} WHERE subject_id = ?"
@@ -230,7 +246,7 @@ class Database:
             self.cursor.execute(query_insert, values)
             self.conn.commit()
             print(f"Subject {subject.subject_id} inserted successfully.")
-        
+
     def build(self):
         self.cursor.execute('''
             CREATE TABLE IF NOT EXISTS Subject (
@@ -281,10 +297,11 @@ class Database:
 
     def wipe(self):
         try:
-            self.cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+            self.cursor.execute(
+                "SELECT name FROM sqlite_master WHERE type='table';")
             table_names = self.cursor.fetchall()
 
-            ## Loop through all the tables and delete all the data
+            # Loop through all the tables and delete all the data
             for table in table_names:
                 table_name = table[0]
                 self.cursor.execute(f"DELETE FROM {table_name};")
@@ -297,13 +314,5 @@ class Database:
 
         return
 
-    
     def close(self):
         self.conn.close()
-
-    
-
-    
-
-    
-
--- setup.py
+++ setup.py
@@ -2,19 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='macaquedb',
-    version='0.1.0',
+    version='0.1.7',
     description='SQLite Database interface for PRIME-DE data',
     author='Sam Alldritt',
     author_email='samuel.alldritt@childmind.org',
-    url='https://github.com/samalldritt/MacaqueDB',
+    url='https://github.com/samalldritt/macaquedb',
     packages=find_packages(),
-    install_requires=[
-        'libgcc-ng>=11.2.0',
-        'libgomp>=11.2.0',
-        'ncurses>=6.4',
-        'readline>=8.2',
-        'sqlite>=3.40.1',
-        'zlib>=1.2.13'
-    ]
+    install_requires=[]
 )
-
